af179b537b5ebbf1df9d0930e0678612c0c4c565
Catherine Hilton <126924004+cat-thebeesknees@users.noreply.github.com>
2023-05-16 11:42:35 -0400

    Add files via upload
    added all files created for block 20 workshop and guided practice.

---
+++ index.js
@@ -0,0 +1,24 @@
+const users = [
+    { name: "John", age: 25, occupation: "gardener" },
+    { name: "Lenny", age: 51, occupation: "programmer" },
+    { name: "Andrew", age: 43, occupation: "teacher" },
+    { name: "Peter", age: 81, occupation: "teacher" },
+    { name: "Anna", age: 43, occupation: "teacher" },
+    { name: "Albert", age: 76, occupation: "programmer" },
+    { name: "Adam", age: 47, occupation: "teacher" },
+    { name: "Robert", age: 72, occupation: "driver" },
+];
+
+function main() {
+
+}
+
+//call the main function
+main();
+// inside your main() function grab the "root" element
+// add an h1 element, give it text of "FREELANCERS"
+// append the HTML document.
+// inside your main() function to create an unordered list element
+// Loop through the users array creating li elements
+// Append those elements to your HTML document
+// Style your site using CSS
---
+++ javascript.js
@@ -0,0 +1,6 @@
+//Use JavaScript to grab the div with an id of root to dynamically add data specified in the Guided Practice
+//Access the root element using JavaScript
+//Create an h1 element with the name of a famous piece of art: Mona Lisa
+//Add (append) the h1 element to the root element
+//Set the image asset and add mona_lisa.jpg image to the root element
+//Add a p element description to the root element. Description should read: The Mona Lisa is a famous painting.
